f8e6965994eca2f3d1f56fd3743ec46fe623533b
xiyuan-fengyu <847639468@qq.com>
2020-12-14 17:40:45 +0800

    启动ElectronService.remote属性

--- src/app/service/electron.service.ts
+++ src/app/service/electron.service.ts
@@ -23,12 +23,11 @@ export class ElectronService {
   constructor() {
     // Conditional imports
     if (this.isElectron) {
-      this.ipcRenderer = window.require('electron').ipcRenderer;
-      this.webFrame = window.require('electron').webFrame;
-
+      const electron = window.require('electron');
+      this.ipcRenderer = electron.ipcRenderer;
+      this.webFrame = electron.webFrame;
       // If you wan to use remote object, pleanse set enableRemoteModule to true in main.ts
-      // this.remote = window.require('electron').remote;
-
+      this.remote = electron.remote;
       this.childProcess = window.require('child_process');
       this.fs = window.require('fs');
     }
